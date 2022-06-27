18bdaac36a55318352201245c6c4acf22270e55f
unknown <thapaprashant69@gmail.com>
2022-06-27 22:03:54 +0545

    Quote Generator complete running files

---
+++ mainScript.js
@@ -0,0 +1,36 @@
+    const heading = document.querySelector("#quote");
+    const author = document.querySelector("#author");
+
+    const fetchData = async()=>{
+        try{
+        const res = await fetch("http://localhost:5000/quotes");
+        const data = await res.json();
+        heading.innerText = data.text;
+        author.innerText = data.author;
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    fetchData();
+
+
+    //Traditional function definition commented here which works then same with try-catch
+    // async function fetchData(){
+    //     const res = await fetch("http://localhost:5000/quotes");
+    //     const data = await res.json();
+    //     heading.innerText = data.text;
+    //     author.innerText=data.author;
+    // }
+
+
+    //Traditional method of fetching data handled with Promises
+    // fetch("http://localhost:5000/quotes").then((res)=>{
+    //     res.json().then((data)=>{
+    //         heading.innerText=data.text;
+    //     }).catch((err)=>{
+    //         console.log(err);
+    //     });
+    // }).catch((err)=>{
+    //     console.log(err);
+    // });
---
+++ quoteGenerator.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const http = require('http');
+
+const generateRandomInteger =(max)=>{
+    return(Math.floor(Math.random()*max)+1);
+}
+
+
+const server = http.createServer((req,res)=>{
+    if(req.url==='/quotes'){
+        fs.readFile("quotesWithAuthors.json","utf8",(err,data)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            let quotes = JSON.parse(data);
+            
+
+            res.writeHead(200,{"Content-Type":"application/json","Access-Control-Allow-Origin":"*"});
+            res.end(JSON.stringify(quotes[generateRandomInteger(quotes.length)]));
+        })
+    }
+
+})
+
+
+server.listen(5000);
+
